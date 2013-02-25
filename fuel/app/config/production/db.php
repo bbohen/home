0f5e618ef83319fda595978407a36da8f2bb9a05
Brad Bohen <bbohen@fullsail.edu>
2013-02-25 17:10:53 -0500

    deletions

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -8,7 +8,7 @@ return array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=home',
 			'username'   => 'root',
-			'password'   => 'hsikeswk',
+			'password'   => 'password',
 		),
 	),
 );
